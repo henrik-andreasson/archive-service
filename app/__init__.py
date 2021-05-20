7b7b6c3f958fb6fd29fc364e7de59900dc6709b5
Henrik Andreasson <henrik.andreasson@cgi.com>
2021-05-20 16:32:04 +0200

    support config file

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,17 @@ from flask_bootstrap import Bootstrap
 from conf.serviceconfig import Config
 from app.log.log import create_logger
 
+
 bootstrap = Bootstrap()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+
     app.config.from_object(config_class)
+    app.config.from_pyfile('archive-service-config.py', silent=True)
+    for conf in app.config:
+        print(f'conf key: {conf} = {app.config[conf]}')
 
     bootstrap.init_app(app)
 
